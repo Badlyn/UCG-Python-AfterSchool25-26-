470c2ecd444241eb11f29c5264e70f949b4eaf67
Badlyn <lucasbishop1224@gmail.com>
2025-11-20 16:56:26 -0800

    Add files via upload

---
+++ LowerUpper.py
@@ -0,0 +1,13 @@
+text = "Hello Class! Python is fun."
+
+lower_text = text.lower()
+print("Lowercase:", lower_text)
+
+upper_text = text.upper()
+print("Uppercase:", upper_text)
+
+replaced_text = text.replace("Fun", "Awesome")
+print("Replace Text:", replaced_text)
+
+combined_text = text.lower().replaced("python", "java")
+print("Combined Methods:", combined_text)
